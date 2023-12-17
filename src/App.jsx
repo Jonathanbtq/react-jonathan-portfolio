066a7e97e6d6e5096110a917185346845fc1cdfa
Jonathanbtq <botquin.jonathan@yahoo.fr>
2023-12-17 19:32:58 +0100

    PortfolioBase

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import { RouterProvider, createBrowserRouter, Outlet, NavLink, useRouteError } from 'react-router-dom'
+import Minecraft from './pages/Minecraft'
+import Home from './pages/Home'
+
+const router = createBrowserRouter([
+  {
+   path: '/',
+   element: <Root />,
+   errorElement: <PageError />,
+   children: [
+     {
+       path: 'home',
+       element: <Home />,
+     },
+     {
+       path: 'minecraft',
+       element: <Minecraft />,
+     },
+     {
+       path: 'contact',
+       element: <div>Page de contact</div>,
+     },
+    //  {
+    //   path: 'mc',
+    //   children: [
+    //     {
+    //       path: '',
+    //       element: <>Mon blog</>
+    //     },
+    //     {
+    //       path: ':id',
+    //       element: <UniquePage />
+    //     }
+    //   ]
+    //  }
+   ]
+  }
+])
+
+function PageError(){
+  const error = useRouteError()
+  return <>
+    <h1>Une erreur est arrivé</h1>
+    <p>
+      {error?.error?.toString() ?? error?.toString()}
+    </p>
+  </>
+}
+
+function Root(){
+  return <>
+    <header>
+      <nav>
+        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/minecraft">Minecraft</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+    </header>
+    <div className="">
+      <Outlet />
+    </div>
+  </>
+}
+
+function App() {
+  return (<RouterProvider router={router}/>)
+}
+
+export default App
---
+++ src/pages/Minecraft.jsx
@@ -0,0 +1,9 @@
+function Minecraft(){
+    return (
+        <>
+            <div className="">Portfolio Minecraft</div>
+        </>
+    )
+}
+
+export default Minecraft;
