59c7d29f0ba951c976de01c8841ed814a77a610c
Jonathanbtq <botquin.jonathan@yahoo.fr>
2024-01-14 17:12:31 +0100

    fix

--- src/components/FormationProjects.jsx
+++ src/components/FormationProjects.jsx
@@ -9,6 +9,10 @@ import bootstrap from '../assets/img/bootstrap.png'
 import php from '../assets/img/phpicon.png'
 
 export default function FormationProjects() {
+    
+    const [scrollX, setScrollX] = useState(0);
+    const containerRef = useRef([])
+    const [images, setImages] = useState([])
 
     function renderStackIcon(stack) {
         const iconMap = {
@@ -34,22 +38,110 @@ export default function FormationProjects() {
         return number % 2 === 0;
     }
 
-    const [scrollX, setScrollX] = useState(0);
-    const containerRef = useRef(null)
+    const handleScroll = (direction, index) => {
+        console.log(index)
+        containerRef.current.map((container) => {
+            if(index === container.id){
+                console.log(container)
+                if(!container) return
 
-    const handleScroll = (direction) => {
-        const container = containerRef.current
-        console.log(container)
-        if(!container) return
+                const containerWidth = container.clientWidth
+                const moveAmount = containerWidth / 2;
 
-        const containerWidth =container.clientWidth
-        const moveAmount = containerWidth / 2;
+                if (direction === "left") {
+                setScrollX((prev) => Math.max(prev - moveAmount, 0));
+                } else if (direction === "right") {
+                setScrollX((prev) => Math.min(prev + moveAmount, container.scrollWidth - containerWidth));
+                }
+            }
+            
+        })
+    }
 
-        if (direction === "left") {
-        setScrollX((prev) => Math.max(prev - moveAmount, 0));
-        } else if (direction === "right") {
-        setScrollX((prev) => Math.min(prev + moveAmount, container.scrollWidth - containerWidth));
-        }
+    const ProjetMapping = () => {
+        return (
+            <React.Fragment >
+                {formaProjects.map((projet, index) => (
+                    isEven(index)
+                    ?
+                    <div className="prt_frm_card" key={index}>
+                        {projet.array_options.map((options, optionsIndex) => (
+                            <div className="prt_frm_crd_ctn" ref={(el) => (containerRef.current[options.id] = el)} key={optionsIndex}>
+                                <p className="left_arrow_forma" onClick={() => handleScroll('left', index)}>left</p>
+                                <ul className="prt_frm_content">
+                                    {options.img.map((img, imgIndex) => (
+                                        <li key={imgIndex}>
+                                            <img src={img} alt={`Image ${imgIndex + 1}`} />
+                                        </li>
+                                    ))}
+                                </ul>
+                                <p className="right_arrow_forma" onClick={() => handleScroll('right')}>right</p>
+                            </div>
+                        ))}
+                        <div className="prt_frm_card_content">
+                            <h3>{projet.name}</h3>
+                            <p>{projet.description}</p>
+                            <p className="lang_use_prt_frm">Langages utilisés :</p>
+                            <div className="prt_frm_card_stack">
+                                {projet.array_options.map((options, optionsIndex) => (
+                                    <React.Fragment  key={optionsIndex}>
+                                        {options.stacks.map((stack, stackIndex) => (
+                                            <div className="prt_frm_crd_icon" key={stackIndex}>
+                                                {stack === 'SYMFONY' ? (
+                                                <img src={symfony} alt={stack} />
+                                                ) : stack === 'CSS' ? (
+                                                <img src={css} alt={stack} />
+                                                ) : stack === 'JS' ? (
+                                                <img src={js} alt={stack} />
+                                                ) : stack === 'HTML' ? (
+                                                    <img src={html} alt={stack} />
+                                                ) : stack === 'BOOTSTRAP' ? (
+                                                    <img src={bootstrap} alt={stack} />
+                                                ) : stack === 'PHP' ? (
+                                                    <img src={php} alt={stack} />
+                                                ) : null}
+                                            </div>
+                                        ))}
+                                    </React.Fragment>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    :
+                    <div className="prt_frm_card" key={index}>
+                        <div className="prt_frm_card_content">
+                            <h3>{projet.name}</h3>
+                            <p>{projet.description}</p>
+                            <p className="lang_use_prt_frm">Langages utilisés :</p>
+                            <div className="prt_frm_card_stack">
+                                {projet.array_options.map((options, optionsIndex) => (
+                                    <React.Fragment  key={optionsIndex}>
+                                        {options.stacks.map((stack, stackIndex) => (
+                                            <div className="prt_frm_crd_icon" key={stackIndex}>
+                                                {stack === 'SYMFONY' ? (
+                                                    <img src={symfony} alt={stack} />
+                                                ) : stack === 'CSS' ? (
+                                                    <img src={css} alt={stack} />
+                                                ) : stack === 'JS' ? (
+                                                    <img src={js} alt={stack} />
+                                                ) : stack === 'HTML' ? (
+                                                    <img src={html} alt={stack} />
+                                                ) : stack === 'BOOTSTRAP' ? (
+                                                    <img src={bootstrap} alt={stack} />
+                                                ) : stack === 'PHP' ? (
+                                                    <img src={php} alt={stack} />
+                                                ) : null}
+                                            </div>
+                                        ))}
+                                    </React.Fragment>
+                                ))}
+                            </div>
+                        </div>
+                        <img src={projet.img} alt="" />
+                    </div>
+                ))}
+            </React.Fragment>
+        )
     }
 
     return (
@@ -60,85 +152,7 @@ export default function FormationProjects() {
                         <h2>Projets de formation</h2>
                         <a href="https://github.com/Jonathanbtq" target="_blank" className="lien_page_index">GitHub</a>
                     </div>
-                    {formaProjects.map((projet, index) => (
-                        isEven(index)
-                            ?
-                            <div className="prt_frm_card" key={index}>
-                                {projet.array_options.map((options, optionsIndex) => (
-                                    <div className="prt_frm_crd_ctn" ref={containerRef} key={optionsIndex}>
-                                        <p className="left_arrow_forma" onClick={() => handleScroll('left')}>left</p>
-                                        <ul className="prt_frm_content">
-                                            {options.img.map((img, imgIndex) => (
-                                                <li>
-                                                    <img src={img} alt={`Image ${imgIndex + 1}`} key={imgIndex} />
-                                                </li>
-                                            ))}
-                                        </ul>
-                                        <p className="right_arrow_forma" onClick={() => handleScroll('right')}>right</p>
-                                    </div>
-                                ))}
-                                <div className="prt_frm_card_content">
-                                    <h3>{projet.name}</h3>
-                                    <p>{projet.description}</p>
-                                    <p className="lang_use_prt_frm">Langages utilisés :</p>
-                                    <div className="prt_frm_card_stack">
-                                        {projet.array_options.map((options, optionsIndex) => (
-                                            <React.Fragment  key={optionsIndex}>
-                                                {options.stacks.map((stack, stackIndex) => (
-                                                    <div className="prt_frm_crd_icon" key={stackIndex}>
-                                                        {stack === 'SYMFONY' ? (
-                                                        <img src={symfony} alt={stack} />
-                                                        ) : stack === 'CSS' ? (
-                                                        <img src={css} alt={stack} />
-                                                        ) : stack === 'JS' ? (
-                                                        <img src={js} alt={stack} />
-                                                        ) : stack === 'HTML' ? (
-                                                            <img src={html} alt={stack} />
-                                                        ) : stack === 'BOOTSTRAP' ? (
-                                                            <img src={bootstrap} alt={stack} />
-                                                        ) : stack === 'PHP' ? (
-                                                            <img src={php} alt={stack} />
-                                                        ) : null}
-                                                  </div>
-                                                ))}
-                                            </React.Fragment>
-                                        ))}
-                                    </div>
-                                </div>
-                            </div>
-                            :
-                            <div className="prt_frm_card" key={index}>
-                                <div className="prt_frm_card_content">
-                                    <h3>{projet.name}</h3>
-                                    <p>{projet.description}</p>
-                                    <p className="lang_use_prt_frm">Langages utilisés :</p>
-                                    <div className="prt_frm_card_stack">
-                                        {projet.array_options.map((options, optionsIndex) => (
-                                            <React.Fragment  key={optionsIndex}>
-                                                {options.stacks.map((stack, stackIndex) => (
-                                                    <div className="prt_frm_crd_icon" key={stackIndex}>
-                                                        {stack === 'SYMFONY' ? (
-                                                            <img src={symfony} alt={stack} />
-                                                        ) : stack === 'CSS' ? (
-                                                            <img src={css} alt={stack} />
-                                                        ) : stack === 'JS' ? (
-                                                            <img src={js} alt={stack} />
-                                                        ) : stack === 'HTML' ? (
-                                                            <img src={html} alt={stack} />
-                                                        ) : stack === 'BOOTSTRAP' ? (
-                                                            <img src={bootstrap} alt={stack} />
-                                                        ) : stack === 'PHP' ? (
-                                                            <img src={php} alt={stack} />
-                                                        ) : null}
-                                                  </div>
-                                                ))}
-                                            </React.Fragment>
-                                        ))}
-                                    </div>
-                                </div>
-                                <img src={projet.img} alt="" />
-                            </div>
-                    ))}
+                    <ProjetMapping />
                 </div>
             </div>
         </>
