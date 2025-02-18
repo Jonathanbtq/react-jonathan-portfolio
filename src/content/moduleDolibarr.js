7421bd718c1f360f491dbe601b81483356b8aee3
Jonathanbtq <botquin.jonathan@yahoo.fr>
2025-02-19 00:50:11 +0100

    add boutique page

--- src/content/moduleDolibarr.js
+++ src/content/moduleDolibarr.js
@@ -3,25 +3,29 @@ const modulesDolibarr = [
         name: 'DoliZipExport',
         image: '/src/assets/img/moduleDolibarr/dolizipexport.png',
         url: '/src/assets/modules/dolizipexport.zip',
-        prix: '150'
+        prix: '150',
+        version: '19 - 20'
     },
     {
         name: 'Qtycheck',
         image: '/src/assets/img/moduleDolibarr/qtycheck.png',
         url: '/src/assets/modules/qtycheck.zip',
-        prix: '30'
+        prix: '30',
+        version: '19 - 20'
     },
     {
         name: 'Refbycustomer',
         image: '/src/assets/img/moduleDolibarr/refbycustomer.png',
         url: "/src/assets/modules/refbycustomer.zip",
-        prix: '120'
+        prix: '120',
+        version: '19 - 20'
     },
     {
         name: 'Freelinedetails',
         image: '/src/assets/img/moduleDolibarr/freelinedetails.png',
         url: "/src/assets/modules/freelinedetails.zip",
-        prix: '250'
+        prix: '250',
+        version: '19 - 20'
     },
 ]
 
--- src/pages/Boutique.jsx
+++ src/pages/Boutique.jsx
@@ -5,7 +5,6 @@ import ScrollToTopButton from "../components/ScrollToTopButton";
 import moduleDolibarr from "../content/moduleDolibarr";
 
 export default function Boutique() {
-    console.log(moduleDolibarr)
     return (
         <div className="boutique_main">
             <div className="boutique_mod_ctn">
@@ -14,6 +13,9 @@ export default function Boutique() {
                     {moduleDolibarr.map((module, key) => {
                         return (
                             <div className="doli_mod_card" key={key}>
+                                <div class="btq_doli_mod_vrs" title="Version Dolibarr (min - max)">
+                                    <span>{module.version} </span>
+                                </div>
                                 <img src={`${window.location.origin}${module.image}`} alt={module.name} />
                                 <section>
                                     <a href={module.url}>
